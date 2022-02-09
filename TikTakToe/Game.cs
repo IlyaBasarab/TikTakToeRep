6584fd3e3220482f2753622b00f4b22971fa7b3d
IlyaBasarab <town_prok@mail.ru>
2022-02-09 11:02:42 +0100

    Přidejte soubory projektu.

---
+++ TikTakToe/Game.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTakToe
+{
+    class Game
+    {
+        private Board board = new Board();
+        private Visual visual = new Visual();
+
+        private Player player1 = null;
+        private Player player2 = null;
+
+        public Game()
+        {
+            
+        }
+
+
+        public void Start()
+        {
+
+            player1 = new Player(1);
+            player2 = new Player(2);
+
+            bool player1Turn = true;
+
+            while(!IsGemeOver())
+            {
+                if(player1Turn)
+                {
+                    player1.Move(board);
+                }
+                else
+                {
+                    player2.Move(board);
+                }
+
+                visual.ShowBoard(board);
+                player1Turn = !player1Turn;
+
+            }
+            
+
+        }
+
+        public bool IsGemeOver()
+        {
+            int element = WinnerCheck();
+
+            if (element == 0)
+                return false;
+            else if (element == 1)
+            {
+                Console.WriteLine("_________ " + visual.GetSimbol( player1.GetPlayer())+ "   wins!   __________________________");
+                return true;
+            }
+            else if (element == 2)
+            {
+                Console.WriteLine("_________" + visual.GetSimbol( player2.GetPlayer()) + "  wins!   ______________________");
+                return true;
+            }
+
+            return false;
+        }
+
+        public int WinnerCheck()
+        {
+
+            try
+            {
+                for (int i = 0; i < board.GetRowCount() - 2; i++)
+                {
+
+                    for (int j = 0; j <= board.GetColCount() - 2; j++)
+                    {
+                        while (!board.IsEmpty(i, j))
+                        {
+                            if (board.GetElem(i, j) == board.GetElem(i + 1, j) && board.GetElem(i + 1, j) == board.GetElem(i + 2, j))
+                                return board.GetElem(i, j);
+
+                            else if (board.GetElem(i, j) == board.GetElem(i, j + 1) && board.GetElem(i, j + 1) == board.GetElem(i, j + 2))
+                                return board.GetElem(i, j);
+
+                            else if (board.GetElem(i, j) == board.GetElem(i + 1, j + 1) && board.GetElem(i + 1, j + 1) == board.GetElem(i + 2, j + 2))
+                                return board.GetElem(i, j);
+
+                            return 0;
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 0;
+            }
+
+            return 0;
+        }
+
+
+
+    }
+}
---
+++ TikTakToe/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTakToe
+{
+    class Board
+    {
+        private int[,] board = new int[3,3];
+
+        public void SetElement(int row, int col,int element)
+        {
+            try
+            {
+                board[row, col] = element;
+            }
+            catch (ArgumentException ex)
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+        }
+
+        public int GetElem(int row, int col)
+        {
+            try
+            {
+                return board[row, col];
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+                return 0;
+            }
+        }
+
+        public int GetRowCount()
+        {
+            return board.GetLength(0);
+        }
+
+        public int GetColCount()
+        {
+            return board.GetLength(1);
+        }
+
+
+        public bool IsEmpty(int row, int col)
+        {
+            try 
+            { 
+                return board[row, col] == 0; 
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex); 
+                return false;                
+            }
+            
+        }
+
+
+
+        public Board()
+        {
+
+        }
+ }
+
+
+
+    class Visual
+    {
+
+        public Visual() 
+        { }
+
+        public void ShowBoard(Board board)
+        {
+
+            for (int i = 0; i < board.GetRowCount(); i++)
+            {
+                for (int j = 0; j < board.GetColCount(); j++)
+                {
+                    int elem = board.GetElem(i, j);
+                    char symbol = GetSimbol(elem);
+
+                    Console.Write(symbol + "\t");
+
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public char GetSimbol(int value)
+        {
+            if (value == 1)
+                return 'X';
+            else if (value == 2)
+                return '0';
+            return '_';
+        }
+
+
+    }
+
+
+    class Player
+    {
+        private int playerSymbol;
+
+        public Player(int symbol)
+        {
+            this.playerSymbol = symbol;
+        }
+
+        public int GetPlayer()
+        {
+            return playerSymbol;
+        }
+
+        public void Move(Board board)
+        {
+            int rowInput = 0;
+            int colInput = 0;
+
+            int rowIdx = 0;
+            int colIdx = 0;
+
+            do
+            {
+                Console.WriteLine("Enter row [1;3]");
+                rowInput = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter col [1;3]");
+                colInput = Convert.ToInt32(Console.ReadLine());
+
+
+                rowIdx = rowInput - 1;
+                colIdx = colInput - 1;
+
+
+            } while (!board.IsEmpty(rowIdx,colIdx));
+
+            board.SetElement(rowIdx, colIdx, playerSymbol);
+        }
+
+
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Game game = new Game();
+            game.Start();
+
+        }
+    }
+}
